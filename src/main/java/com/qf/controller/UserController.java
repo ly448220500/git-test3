3e49e07d9ec3669fc08c851639e4f0c592441482
liyang <448220500@qq.com>
2019-09-26 17:21:00 +0800

    aaaaaa

---
+++ src/main/java/com/qf/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "test")
+public class UserController {
+
+    public static void main(String[] args) {
+
+    }
+}
